721c86bdbac72854cf73b6b7fe527a2285a5a00a
Ayush Aggarwal <ayushaggarwal8888@gmail.com>
2026-04-14 14:13:54 -0500

    small fix

--- src/client/engine/render/renderer.ts
+++ src/client/engine/render/renderer.ts
@@ -156,6 +156,16 @@ export class Renderer {
       undefined,
       cube.normalsFlat(),
     );
+    pass.addAttribute(
+      "aUV",
+      2,
+      gl.FLOAT,
+      false,
+      2 * Float32Array.BYTES_PER_ELEMENT,
+      0,
+      undefined,
+      cube.uvFlat(),
+    );
     pass.addInstancedAttribute(
       "aOffset",
       4,
