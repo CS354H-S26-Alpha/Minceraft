55903a4036458acbdd92a1400b353aa56e2d59b5
Ayush Aggarwal <101430874+ayushaggarwal8@users.noreply.github.com>
2026-04-14 16:13:59 -0500

    Revert "Block textures (#50)"
    This reverts commit eb8d16b2bb16ea18a18941a96bd23ebd84f48d69.

--- src/client/engine/render/renderer.ts
+++ src/client/engine/render/renderer.ts
@@ -156,7 +156,6 @@ export class Renderer {
       undefined,
       cube.normalsFlat(),
     );
-    pass.addAttribute("aUV", 2, gl.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0, undefined, cube.uvFlat());
     pass.addInstancedAttribute(
       "aOffset",
       4,
